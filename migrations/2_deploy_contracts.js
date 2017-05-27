ff7c8ab5ea60741d5057f33de0cf176055d14c76
gabrielghost <charlie.gavins@neonseed.com>
2017-05-27 12:52:11 +0100

    first commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,7 @@
+var ProofOfExistence3 = artifacts.require("./ProofOfExistence3.sol");
+/*
+ * migrations/2_deploy_contracts.js:
+ */
+module.exports = function(deployer) {
+  deployer.deploy(ProofOfExistence3);
+};
